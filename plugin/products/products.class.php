90b1bf445a30018950a6c41db742d9d28d55761b
cOreey <koritakjan@gmail.com>
2013-10-17 19:43:37 +0200

    Doplněna třída products. Metoda vypisProduktu() nyni vypisuje  vsechny produkty/produkty dane kategorie. Vypis je formatovan do tri-sloupcove tabulky. Metoda detailProduktu() vypise jmeno_produktu, kategorii a popis_produktu pro vybraný produkt.

--- plugin/products/products.class.php
+++ plugin/products/products.class.php
@@ -1,8 +1,10 @@
-<?php
+﻿<?php
 
 class Products {
 
 	private $output = "";
+	private $resultSet;
+	private $single;
 
 	public static $instance = "";
 	public static $instanceCount = 0;
@@ -25,7 +27,7 @@ class Products {
 
 		switch($operation_id) {
 			case 1:
-				$this->output = $this->vypisProduktu();
+				$this->vypisProduktu();
 				break;
 			case 2:
 				$this->detailProduktu();
@@ -35,15 +37,70 @@ class Products {
 
 	private function vypisProduktu() {
 
-		$output = "Vypis produktu";
+		$td_counter = 0;
+		$id_produktu = 0;
 
-		return $output;
+		if(isset($_GET['kategorie']))
+			web::$db->query("SELECT id,jmeno_produktu FROM love_eshop_produkt WHERE produkt_kategorie =" .$_GET['kategorie']);
+		else
+			web::$db->query("SELECT id,jmeno_produktu FROM love_eshop_produkt");
+
+		$this->resultSet = web::$db->resultset();
+
+		$this->output .= "<table>";
+		$this->output .= "<tr>";
+
+		foreach($this->resultSet as $row) {
+
+			if($td_counter == 3)
+				$this->output .= "<tr>";		
+
+			$this->output .= "<td>";
+			$this->output .= "<div class=\"produkt\">";
+
+			foreach($row as $key => $value) {
+				if($key == 'id')
+					$id_produktu = $value;
+				else
+					$this->output .= "<a href=\"?produkt=" .$id_produktu. "\">" .$value. "</a>";
+			}
+
+			$this->output .= "</div>";
+			$this->output .= "</td>";
+
+			$td_counter++;
+
+			if($td_counter == 3) {
+				$this->output .= "</tr>";
+				$td_counter = 0;
+			}
+		}
+
+		if($td_counter != 0)
+			$this->output .= "</tr>";	
+
+		$this->output .= "</table>";
+
+		return $this->output;
 
 	}
 
 	private function detailProduktu() {
 
-		$this->output = "Detail produktu";
+		web::$db->query("SELECT jmeno_produktu, kategorie, popis_produktu FROM love_eshop_produkt WHERE id =" .$_GET['produkt']);
+
+		$this->resultSet = web::$db->single();
+
+		$this->output .= "<div>";
+
+		foreach($this->resultSet as $key => $value) {
+				$this->output .= $value;
+				$this->output .= "</br>";
+		}
+
+		$this->output .= "</div>";
+
+		return $this->output;
 	}
 
 
