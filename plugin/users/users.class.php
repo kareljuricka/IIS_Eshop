69fced9e544aa1f68af5630f40cb54f02631079c
Karel Juricka <kapa@loveart.cz>
2013-10-19 11:28:19 +0200

    Dokončeno upravení uživatele, oddělení funckionality od tvorby z důvodu lepší předhlednosti zdrojového kódu. Připravena sekce na změnu hesla uživatele.

--- plugin/users/users.class.php
+++ plugin/users/users.class.php
@@ -1,7 +1,9 @@
 <?php
 
 define("REGISTER_FORM", 0);
-define("REGISTER_SUCCESS", 1);
+define("UPDATE_FORM", 1);
+define("REGISTER_SUCCESS", 2);
+define("UPDATE_SUCCESS", 3);
 
 class Users {
 
@@ -9,7 +11,6 @@ class Users {
 
 	public static $instance = "";
 	public static $instanceCount = 0;
-
 
 	public function __construct($operation_id) {
 
@@ -27,20 +28,30 @@ class Users {
 	public function pluginProcess($operation_id) {
 
 		switch($operation_id) {
+			
+			// Registrace
 			case 1:
 				$this->output = $this->registerUser();
 				break;
 
+			// Přihlášení
 			case 2:
 				$this->output = $this->loginUser();
 				break;
 
+			// Uživatelský panel
 			case 3:
 				$this->output = $this->userPanel();
 				break;
 
+			// Úprava osobních dat
 			case 4:
-				$this->output = $this->registerUser($_SESSION['user-id']);
+				$this->output = $this->updateUser();
+				break;
+
+			// Úprava hesla
+			case 5:
+				$this->output = $this->passwordChange();
 				break;
 
 		}
@@ -58,7 +69,9 @@ class Users {
 		if (isset($_SESSION['user-id'])) {
 			$output = "Přihlášeno <br />
 			<a href='".web::$serverDir."upravit-osobni-udaje' title='upravit'>Upravit osobní údaje</a><br />
-			<a href='".web::$serverDir."?page=".$act_page."&action=logout' title='log out'>Odhlásit</a>";
+			<a href='".web::$serverDir."zmena-hesla' title='zmenit heslo'>Změnit heslo</a><br />
+			<a href='".web::$serverDir."?page=".$act_page."&action=logout' title='log out'>Odhlásit</a><br />
+			";
 			
 			// Odhlaseni uzivatele
 			if (isset($_GET['action']) && $_GET['action'] == 'logout') {
@@ -77,138 +90,246 @@ class Users {
 	/* Registracni formular */
 	private function registerUser($update_id = NULL) {
 
-		$state = REGISTER_FORM;
-		$error_output = "";
+		try {
+			if (isset($_SESSION['user-id']))
+				throw new Exception('Already registred');
 
-		$userdata = array('email' => '', 'heslo' => '', 'jmeno' => '', 'prijmeni' => '', 'mobil' => '', 'ulice' => '',
+			$userdata = array('email' => '', 'heslo' => '', 'jmeno' => '', 'prijmeni' => '', 'mobil' => '', 'ulice' => '',
 			'cislo_popisne' => '', 'mesto' => '', 'psc' => '', 'aktivni' => '', 'novinky' => ''
-		);
+			);
 
-		if (!isset($update_id)) {
-			$title = "Registrace uživatelů";
-			$submit_value = "Registrovat";
-		}
-		else {
-			$title = "Upravit osobní údaje";
-			$submit_value = "Upravit";
-		}
+			$state = REGISTER_FORM;
+			$error_output = "";
+			$password_input = "";
 
-		if (isset($_POST['register_update'])) {	
+			if (isset($_POST['register_update'])) {	
 
-			// Get POST data
-			foreach($_POST as $key => $value) {
-				if (array_key_exists($key, $userdata))
-					$userdata[$key] = $value;
-			}
+				// Get POST data
+				foreach($_POST as $key => $value) {
+					if (array_key_exists($key, $userdata))
+						$userdata[$key] = $value;
+				}
 
-			// Check errors
-			if (empty($_POST['email'])) $this->errors['register'][] = "Nevyplněná emailová adresa";
-			if (empty($_POST['heslo']) || empty($_POST['heslo2'])) $this->errors['register'][] = "Nevyplněné heslo nebo heslo pro kontrolu";
-			if ($_POST['heslo'] != $_POST['heslo2']) $this->errors['register'][] = "Zadané hesla se liší";
+				// Check errors
+				if (empty($_POST['email'])) $this->errors['register'][] = "Nevyplněná emailová adresa";
 
-			// If no errors
-			if (!empty($this->errors['register'])) {
-				$error_output = $this->getErrors();
-
-			}
-			else {
-					
-				$novinky_mail = (isset($_POST['novinky']) && $_POST['novinky']) ? 1 : 0;
-
+				// Při registraci zkontroluj hesla
 				if (!isset($update_id)) {
+					if (empty($_POST['heslo']) || empty($_POST['heslo2'])) $this->errors['register'][] = "Nevyplněné heslo nebo heslo pro kontrolu";
+					if ($_POST['heslo'] != $_POST['heslo2']) $this->errors['register'][] = "Zadané hesla se liší";	
+				}
+
+				// If no errors
+				if (!empty($this->errors['register']))
+					$error_output = $this->getErrors();
+
+				else {
+						
+					$novinky_mail = (isset($_POST['novinky']) && $_POST['novinky']) ? 1 : 0;
+
 					web::$db->query("INSERT INTO " .database::$prefix . "eshop_uzivatel	(email, heslo, jmeno, prijmeni,
 						mobil, ulice, cislo_popisne, mesto, psc, aktivni, novinky)
 						VALUES (:email, :heslo, :jmeno, :prijmeni, :mobil, :ulice, :cislo_popisne, :mesto, :psc,
 						:aktivni, :novinky)");
-					
+						
 					$output = "Registrace byla úspěšná";
 					$state = REGISTER_SUCCESS;
+
+					web::$db->bind(":email", htmlspecialchars($_POST['email']));
+					web::$db->bind(":heslo", htmlspecialchars($_POST['heslo']));
+					web::$db->bind(":jmeno", htmlspecialchars($_POST['jmeno']));
+					web::$db->bind(":prijmeni", htmlspecialchars($_POST['prijmeni']));
+					web::$db->bind(":mobil", htmlspecialchars($_POST['mobil']));
+					web::$db->bind(":ulice", htmlspecialchars($_POST['ulice']));
+					web::$db->bind(":cislo_popisne", htmlspecialchars($_POST['cislo_popisne']));
+					web::$db->bind(":mesto", htmlspecialchars($_POST['mesto']));
+					web::$db->bind(":psc", htmlspecialchars($_POST['psc']));
+					web::$db->bind(":aktivni", 0);
+					web::$db->bind(":novinky", $novinky_mail);
+
+					web::$db->execute();
 				}
-				else {
-					web::$db->query("UPDATE ". database::$prefix ."eshop_uzival SET email = :email, heslo = :heslo,
-						jmeno = :jmeno, prijmeni = :prijmeni, mobil = :mobil, ulice = :ulice, cislo_popisne = :cislo_popisne,
-						mesto = :mesto, psc = :psc, novinky = :novinky WHERE id = '".$update_id."'");
-					$output = "Údaje byly úspěšně upraveny";
-					$state = UPDATE_SUCCESS;
-				}
-
-				web::$db->bind(":email", htmlspecialchars($_POST['email']));
-				web::$db->bind(":heslo", htmlspecialchars($_POST['heslo']));
-				web::$db->bind(":jmeno", htmlspecialchars($_POST['jmeno']));
-				web::$db->bind(":prijmeni", htmlspecialchars($_POST['prijmeni']));
-				web::$db->bind(":mobil", htmlspecialchars($_POST['mobil']));
-				web::$db->bind(":ulice", htmlspecialchars($_POST['ulice']));
-				web::$db->bind(":cislo_popisne", htmlspecialchars($_POST['cislo_popisne']));
-				web::$db->bind(":mesto", htmlspecialchars($_POST['mesto']));
-				web::$db->bind(":psc", htmlspecialchars($_POST['psc']));
-				web::$db->bind(":aktivni", 0);
-				web::$db->bind(":novinky", $novinky_mail);
-
-				web::$db->execute();
-
 			}
 
+			if ($state == REGISTER_FORM) {
+				$output = "
+					<h2>Registrace uživatelů</h2>
+					".$error_output."
+					<form method='POST'>
+						<fieldset>
+							<legend>Přihlašovací údaje</legend>
+							<div>
+								<label for='email'>*Email:</label><input type='text' name='email' id='email' value='".$userdata['email']."'/>
+							</div>
+							<div>
+								<label for='heslo'>*Heslo:</label><input type='password' name='heslo' id='heslo'/>
+								<label for='heslo2'>*Heslo pro kontrolu:</label><input type='password' name='heslo2' id='heslo2'/>
+							</div>
+						</fieldset>
+						<fieldset>
+							<legend>Osobní údaje</legend>
+							<div>
+								<label for='jmeno'>Jméno:</label><input type='text' name='jmeno' id='jmeno' value='".$userdata['jmeno']."'/>
+								<label for='prijmeni'>Přijmení:</label><input type='text' name='prijmeni' id='prijmeni' value='".$userdata['prijmeni']."'/>
+							</div>
+							<div>
+								<label for='mobil'>Mobil:</label><input type='text' name='mobil' id='mobil' value='".$userdata['mobil']."'/>
+							</div>
+							<div>
+								<label for='ulice'>Ulice:</label><input type='text' name='ulice' id='ulice' value='".$userdata['ulice']."' />
+								<label for='cislo_popisne'>Číslo popisné</label><input type='text' name='cislo_popisne' id='cislo_popisne' value='".$userdata['cislo_popisne']."'/>
+							</div>
+							<div>
+								<label for='mesto'>Město:</label><input type='text' name='mesto' id='mesto' value='".$userdata['mesto']."'/>
+								<label for='psc'>PSČ:</label><input type='text' name='psc' id='psc' value='".$userdata['psc']."'/>
+							</div>
+						</fieldset>
+						<fieldset>
+							<legend>Nastavení</legend>
+							<div>
+								<label for='novinky'>Přeji si přijímat novinky emailem:</label><input type='checkbox' name='novinky' id='novinky' ".($userdata['novinky'] ? "checked" : "")."/>
+							</div>
+						</fieldset>
+						<div><input type='submit' value='Registrovat' name='register_update'/></div>
+					</form>";
+			}
 		}
-		else if (isset($update_id)) {
-
-			web::$db->query("SELECT  
-					email, heslo, jmeno, prijmeni, mobil, ulice, cislo_popisne, mesto, psc, aktivni, novinky
-				FROM
-					" .database::$prefix ."eshop_uzivatel
-					
-				WHERE
-					id='".$update_id."'");
-
-			$userdata = web::$db->single();
+		catch (Exception $e) {
+			$output = $e->getMessage();
 		}
-
-		if ($state == REGISTER_FORM) {
-			$output = "
-				<h2>".$title."</h2>
-				".$error_output."
-				<form method='POST'>
-					<fieldset>
-						<legend>Přihlašovací údaje</legend>
-						<div>
-							<label for='email'>*Email:</label><input type='text' name='email' id='email' value='".$userdata['email']."'/>
-						</div>
-						<div>
-							<label for='heslo'>*Heslo:</label><input type='password' name='heslo' id='heslo'/>
-							<label for='heslo2'>*Heslo pro kontrolu:</label><input type='password' name='heslo2' id='heslo2'/>
-						</div>
-					</fieldset>
-					<fieldset>
-						<legend>Osobní údaje</legend>
-						<div>
-							<label for='jmeno'>Jméno:</label><input type='text' name='jmeno' id='jmeno' value='".$userdata['jmeno']."'/>
-							<label for='prijmeni'>Přijmení:</label><input type='text' name='prijmeni' id='prijmeni' value='".$userdata['prijmeni']."'/>
-						</div>
-						<div>
-							<label for='mobil'>Mobil:</label><input type='text' name='mobil' id='mobil' value='".$userdata['mobil']."'/>
-						</div>
-						<div>
-							<label for='ulice'>Ulice:</label><input type='text' name='ulice' id='ulice' value='".$userdata['ulice']."' />
-							<label for='cislo_popisne'>Číslo popisné</label><input type='text' name='cislo_popisne' id='cislo_popisne' value='".$userdata['cislo_popisne']."'/>
-						</div>
-						<div>
-							<label for='mesto'>Město:</label><input type='text' name='mesto' id='mesto' value='".$userdata['mesto']."'/>
-							<label for='psc'>PSČ:</label><input type='text' name='psc' id='psc' value='".$userdata['psc']."'/>
-						</div>
-					</fieldset>
-					<fieldset>
-						<legend>Nastavení</legend>
-						<div>
-							<label for='novinky'>Přeji si přijímat novinky emailem:</label><input type='checkbox' name='novinky' id='novinky' ".($userdata['novinky'] ? "checked" : "")."/>
-						</div>
-					</fieldset>
-					<div><input type='submit' value='".$submit_value."' name='register_update'/></div>
-				</form>
-			";
-		}
-
 
 		return $output;
 
+	}
+
+	/* Update formular */
+	private function updateUser() {
+
+		try {
+			if (!isset($_SESSION['user-id']))
+				throw new Exception('Unautorized access');
+
+			$userdata = array('email' => '', 'jmeno' => '', 'prijmeni' => '', 'mobil' => '', 'ulice' => '',
+			'cislo_popisne' => '', 'mesto' => '', 'psc' => '', 'novinky' => ''
+			);
+
+			$update_id = $_SESSION['user-id'];
+
+			$state = UPDATE_FORM;
+			$error_output = "";
+			$password_input = "";
+
+			web::$db->query("SELECT email, jmeno, prijmeni, mobil, ulice, cislo_popisne, mesto, psc, novinky
+				FROM " .database::$prefix ."eshop_uzivatel WHERE id='".$update_id."'");
+			
+			$userdata = web::$db->single();
+
+			if (isset($_POST['register_update'])) {	
+
+				// Check errors
+				if (empty($_POST['email'])) $this->errors['update'][] = "Nevyplněná emailová adresa";
+
+				// If no errors
+				if (!empty($this->errors['update']))
+					$error_output = $this->getErrors();
+
+				else {
+						
+					$novinky_mail = (isset($_POST['novinky']) && $_POST['novinky']) ? 1 : 0;
+
+				
+					web::$db->query("UPDATE ". database::$prefix ."eshop_uzivatel SET email = :email,
+						jmeno = :jmeno, prijmeni = :prijmeni, mobil = :mobil, ulice = :ulice, cislo_popisne = :cislo_popisne,
+						mesto = :mesto, psc = :psc, novinky = :novinky WHERE id = '".$update_id."'");
+					$output = "Údaje byly úspěšně upraveny";
+
+					web::$db->bind(":email", htmlspecialchars($_POST['email']));
+					web::$db->bind(":jmeno", htmlspecialchars($_POST['jmeno']));
+					web::$db->bind(":prijmeni", htmlspecialchars($_POST['prijmeni']));
+					web::$db->bind(":mobil", htmlspecialchars($_POST['mobil']));
+					web::$db->bind(":ulice", htmlspecialchars($_POST['ulice']));
+					web::$db->bind(":cislo_popisne", htmlspecialchars($_POST['cislo_popisne']));
+					web::$db->bind(":mesto", htmlspecialchars($_POST['mesto']));
+					web::$db->bind(":psc", htmlspecialchars($_POST['psc']));
+					web::$db->bind(":novinky", $novinky_mail);
+
+					web::$db->execute();
+
+					$state = UPDATE_SUCCESS;
+				}
+			}
+
+			if ($state == UPDATE_FORM) {
+				$output = "
+					<h2>Upravit osobní údaje</h2>
+					".$error_output."
+					<form method='POST'>
+						<fieldset>
+							<legend>Přihlašovací údaje</legend>
+							<div>
+								<label for='email'>*Email:</label><input type='text' name='email' id='email' value='".$userdata['email']."'/>
+							</div>
+						</fieldset>
+						<fieldset>
+							<legend>Osobní údaje</legend>
+							<div>
+								<label for='jmeno'>Jméno:</label><input type='text' name='jmeno' id='jmeno' value='".$userdata['jmeno']."'/>
+								<label for='prijmeni'>Přijmení:</label><input type='text' name='prijmeni' id='prijmeni' value='".$userdata['prijmeni']."'/>
+							</div>
+							<div>
+								<label for='mobil'>Mobil:</label><input type='text' name='mobil' id='mobil' value='".$userdata['mobil']."'/>
+							</div>
+							<div>
+								<label for='ulice'>Ulice:</label><input type='text' name='ulice' id='ulice' value='".$userdata['ulice']."' />
+								<label for='cislo_popisne'>Číslo popisné</label><input type='text' name='cislo_popisne' id='cislo_popisne' value='".$userdata['cislo_popisne']."'/>
+							</div>
+							<div>
+								<label for='mesto'>Město:</label><input type='text' name='mesto' id='mesto' value='".$userdata['mesto']."'/>
+								<label for='psc'>PSČ:</label><input type='text' name='psc' id='psc' value='".$userdata['psc']."'/>
+							</div>
+						</fieldset>
+						<fieldset>
+							<legend>Nastavení</legend>
+							<div>
+								<label for='novinky'>Přeji si přijímat novinky emailem:</label><input type='checkbox' name='novinky' id='novinky' ".($userdata['novinky'] ? "checked" : "")."/>
+							</div>
+						</fieldset>
+						<div><input type='submit' value='Upravit' name='register_update'/></div>
+					</form>
+				";
+			}
+
+		}
+		catch (Exception $e) {
+			$output = $e->getMessage();
+		}
+		
+
+		return $output;
+
+	}
+
+	private function passwordChange () {
+
+		$error_output = "";
+
+		$output = "
+			<h2>Změnit heslo</h2>
+			".$error_output."
+			<form method='POST'>
+				<fieldset>
+					<legend>Přihlašovací údaje</legend>
+					<div>
+						<label for='heslo_old'>*Aktuální heslo:</label><input type='password' name='heslo_old' id='heslo_old'/>
+					</div>
+					<div>
+						<label for='heslo'>*Heslo:</label><input type='password' name='heslo' id='heslo'/>
+						<label for='heslo2'>*Heslo pro kontrolu:</label><input type='password' name='heslo2' id='heslo2'/>
+					</div>
+				</fieldset>
+				<div><input type='submit' value='Změnit heslo' name='password_change'/></div>
+			</form>";
+		return $output;
 	}
 
 	/* Prihlasovaci formular */
